a714df19df191c0fa8f8b535b966ec004f6272b7
aillieo <aillieo@163.com>
2017-09-12 20:26:20 +0800

    update

--- Classes/PassString/SceneSendText.cpp
+++ Classes/PassString/SceneSendText.cpp
@@ -15,6 +15,7 @@
 #endif
 
 USING_NS_CC;
+using namespace cocos2d::ui;
 
 SceneBase* SceneSendText::createScene()
 {
@@ -47,6 +48,23 @@ bool SceneSendText::init()
     this->addChild(menu, 1);
     
     
+    
+    
+    m_str = "";
+    m_editBox = cocos2d::ui::EditBox::create(Size(250,50), Scale9Sprite::create("HelloWorld.png"));
+    m_editBox->setPosition(origin + Point(visibleSize.width/2, visibleSize.height*3/4));
+    m_editBox->setFontColor(Color3B(0,0,0));
+    m_editBox->setPlaceHolder("input here");
+    m_editBox->setMaxLength(100);
+    m_editBox->setInputMode(cocos2d::ui::EditBox::InputMode::SINGLE_LINE);
+    m_editBox->setInputFlag(cocos2d::ui::EditBox::InputFlag::INITIAL_CAPS_WORD);
+    m_editBox->setReturnType(cocos2d::ui::EditBox::KeyboardReturnType::DONE);
+    m_editBox->setDelegate(this);
+    addChild(m_editBox);
+    
+    
+    
+    
     return true;
 }
 
@@ -57,6 +75,28 @@ void SceneSendText::menuCallback(Ref* pSender)
     sendText();
     
 }
+
+
+void SceneSendText::editBoxEditingDidBegin(cocos2d::ui::EditBox* editBox)
+{
+    
+}
+
+void SceneSendText::editBoxEditingDidEnd(cocos2d::ui::EditBox* editBox)
+{
+    
+}
+
+void SceneSendText::editBoxTextChanged(cocos2d::ui::EditBox* editBox,const std::string &text)
+{
+    
+}
+
+void SceneSendText::editBoxReturn(cocos2d::ui::EditBox *editBox)
+{
+    
+}
+
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
 void SceneSendText::sendText()
--- Classes/PassString/SceneSendText.hpp
+++ Classes/PassString/SceneSendText.hpp
@@ -11,8 +11,9 @@
 
 #include "cocos2d.h"
 #include "SceneBase.h"
+#include "cocos-ext.h"
 
-class SceneSendText : public SceneBase
+class SceneSendText : public SceneBase, public cocos2d::ui::EditBoxDelegate
 {
 public:
     static SceneBase* createScene();
@@ -27,7 +28,18 @@ private:
     // implement the "static create()" method manually
     CREATE_FUNC(SceneSendText);
 
+    
+    // EditBoxDelegate
+    void editBoxEditingDidBegin(cocos2d::ui::EditBox* editBox);
+    void editBoxEditingDidEnd(cocos2d::ui::EditBox* editBox);
+    void editBoxTextChanged(cocos2d::ui::EditBox* editBox,const std::string &text);
+    void editBoxReturn(cocos2d::ui::EditBox *editBox);
 
+    // EditBox
+    cocos2d::ui::EditBox* m_editBox;
+    std::string m_str;
+    
+    
     void sendText();
     
 };
