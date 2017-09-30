ff5c7170aa93be0589ea63a8b6a1b88aa69498fe
aillieo <aillieo@163.com>
2017-09-30 14:16:27 +0800

    update

--- Classes/Image/SceneGetImage.cpp
+++ Classes/Image/SceneGetImage.cpp
@@ -78,4 +78,42 @@ void SceneGetImage::onImagePickEnd(EventCustom* event)
     
 }
 
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+void SceneGetImage::selectFromPhotos()
+{
+    JniMethodInfo t;
+    cocos2d::log("will try call selectFromPhotos()");
+    if(JniHelper::getStaticMethodInfo(t, CLASS_NAME, "selectFromPhotos", "()V"))
+    {
+        cocos2d::log("has this static method selectFromPhotos()");
+        t.env->CallStaticVoidMethod(t.classID,t.methodID);
+    }
+    
+}
 
+void SceneGetImage::takeWithCamera()
+{
+    JniMethodInfo t;
+    cocos2d::log("will try call takeWithCamera()");
+    if(JniHelper::getStaticMethodInfo(t, CLASS_NAME, "takeWithCamera", "()V"))
+    {
+        cocos2d::log("has this static method takeWithCamera()");
+        t.env->CallStaticVoidMethod(t.classID,t.methodID);
+    }
+    
+}
+
+
+// java to c++ callback
+extern "C"
+{
+    void Java_cocos2d_x_sdk_integration_SceneGetImage_onImageSaved(JNIEnv *env,jobject thiz,jstring path)
+    {
+        std::string strPath = JniHelper::jstring2string(path);
+        Director::getInstance()->getScheduler()->performFunctionInCocosThread([=,strPath]()mutable{
+            Director::getInstance()->getEventDispatcher()->dispatchCustomEvent("ImagePickEnd",&strPath);
+        });
+    }
+}
+
+#endif
